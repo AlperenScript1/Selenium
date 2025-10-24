6f2f9dfc9b3ffff6a8a9256c21af0bfa7945353e
Alperen <alperensenscript@gmail.com>
2025-10-24 16:16:26 +0300

    New project cookieClicker

---
+++ Projeler/cookieClicker.py
@@ -0,0 +1,33 @@
+from selenium import webdriver #Selenium web için gerekli driver kurulumu. 
+import chromedriver_autoinstaller #Otomatik tarayıcı sürümünü güncellemesi için gerekli kütüphane
+from selenium.webdriver.common.by import By
+from time import sleep
+
+chromedriver_autoinstaller;
+
+driver = webdriver.Chrome(); 
+driver.set_window_size(1920, 800);
+driver.implicitly_wait(10);
+
+url = "https://orteil.dashnet.org/cookieclicker/"; 
+driver.get(url); 
+
+try:
+   selectLang = driver.find_element(By.ID,"langSelect-EN");
+   selectLang.click();
+   print("Find: lang");
+   sleep(1);
+   selectCookie = driver.find_element(By.ID,"bigCookie");
+
+   i = 0;
+   while i != 100:
+       selectCookie.click();
+       i+=1
+       print(str(i) +". defa Tıklandı :)");
+       selectCookie.click();
+       
+except:
+    print("Err..");
+
+sleep(1);
+driver.quit();
