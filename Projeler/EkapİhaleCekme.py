93b80b1983c8034266c7a01c6a7f9ba34df00185
Alperen <alperensenscript@gmail.com>
2025-10-31 14:45:22 +0300

    Ekap filtre seçimi
    Added filter selection for tender types and improved scrolling logic.

--- Projeler/EkapİhaleCekme.py
+++ Projeler/EkapİhaleCekme.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
-from time import sleep
+from time import sleep, time
 chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
@@ -11,6 +11,18 @@ driver.implicitly_wait(10);
 driver.get("https://ekap.kik.gov.tr/EKAP/YeniIhaleArama.aspx?qs=1&dt=true");
 ekapListeSonuc = []; #? Bulduğum değerleri atacağım bir liste oluşturuyorum.
     
+#!Filtre değiştirmek için Mal/Yapım/Hizmet for='rdIhaleTur_1' kısmından seçim yapılabilir.
+
+#TODO: for='rdIhaleTur_1' => Mal
+#TODO: for='rdIhaleTur_2' => Yapım
+#TODO: for='rdIhaleTur_3' => Hizmet
+
+filtreAdi = driver.find_element(By.CSS_SELECTOR, "label[for='rdIhaleTur_1']").get_attribute("innerHTML"); #? Ekap sayfasından filtreler kısmından hangi filtreyi seçtiğimi alıyorum.
+filtre = driver.find_element(By.CSS_SELECTOR, "label[for='rdIhaleTur_1']").click(); #? Ekap sayfasından filtreler kısmından 'Yapım' seçiyorum.
+filtreleButton = driver.find_element(By.ID, "btnFilter").click(); #? Filtreleme butonuna tıklıyorum.
+sleep(5); #? Filtreleme için bekliyorum.
+print("Secilen filtre: " + str(filtreAdi));
+
 son_Yukseklik = driver.execute_script("return document.body.scrollHeight") #? Başlangıçta sayfa yüksekliğini al.
 i = 0;
 while True: #? Kırılana kadar tekrar eden döngü.
@@ -21,11 +33,11 @@ while True: #? Kırılana kadar tekrar eden döngü.
     i += 1; #= i değerini artırıyorum ki bir sonraki elemanı alabilmek için.
 
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") #? Sayfanın en altına kaydır.
-    sleep(5); #? Yeni içerik yüklenmesi için kısa bekleme.
+    sleep(1); #? Yeni içerik yüklenmesi için kısa bekleme.
     
     yeni_Yukseklik = driver.execute_script("return document.body.scrollHeight"); #? Yeni yüksekliği al.
     if yeni_Yukseklik == son_Yukseklik: #? Eğer yükseklik değişmediyse, sayfanın sonuna geldik.
-        print("Sayfanın sonuna ulaşıldı!" + "Toplam alınan ihale sayisi: " + str(i));
+        print("\nSayfanın sonuna geldiniz.\n \n" + "Toplam alınan ihale sayisi: " + str(i) + "\n");
         print(ekapListeSonuc);
         break
 
